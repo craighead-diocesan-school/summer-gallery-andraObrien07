b95b653949102cce9a20698a29c867b4567708ef
andraObrien07 <comanan09@craighead.school.nz>
2024-02-09 14:56:05 +1300

    footer
    added a footer componenmt with bulma

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>&copy; Craighead Diocesan School 2024</p>
+  </div>
+</footer>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,13 @@
-<header>
-  <h1 class="title">Welcome</h1>
+<header class="hero is-danger">
+  <div class="hero-body">
+    <h1 class="title">Welcome</h1>
+    <h2 class="subtitle">Emma is great</h2>
+  </div>
 </header>
 
-<style>
+<!-- <style>
   header {
     background-color: orange;
+    font-family: "Times New Roman", Times, serif;
   }
-</style>
+</style> -->
