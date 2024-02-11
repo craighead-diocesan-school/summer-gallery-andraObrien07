01335b7343e8b619be68f27d8b495fc82139ecf4
andraObrien07 <comanan09@craighead.school.nz>
2024-02-12 10:29:20 +1300

    nav
    second page that reuses coimpoonetnts

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -2,3 +2,11 @@
     <a href="/"> Home </a>
     <a href="/about-me"> About Me </a>
 </nav>
+
+<style>
+    nav {
+        text-decoration: line-through;
+        text-shadow: 2px 2px orange;
+        text-align: center;
+    }
+</style>
