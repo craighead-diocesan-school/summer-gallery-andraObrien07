98677b9d4f6c5ca82158d9e58fc64be5b7138db1
andraObrien07 <comanan09@craighead.school.nz>
2024-02-12 10:43:03 +1300

    added a nav page
    connected tothe hoke page with re used components

---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
+    import Nav from "$lib/Nav.svelte";
+</script>
+
+<Header />
+<Nav />
+<Footer />
+
+<!-- <header class="hero is-danger">
+    <div class="hero-body">
+        <h1 class="title">Welcome</h1>
+        <h2 class="subtitle">about me</h2>
+    </div>
+</header> -->
+
+<main></main>
