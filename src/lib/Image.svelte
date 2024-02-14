5c9ba5490f349ea4f119966332f376d48f2bdde1
andraObrien07 <comanan09@craighead.school.nz>
2024-02-14 21:14:41 +1300

    attemp in image props
    I will try again tomorrow

---
+++ src/lib/Image.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let src = "";
+    export let alt = "";
+    export let caption = "";
+</script>
+
+<figure>
+    <img {src} {alt} />
+    <figcaption>{caption}</figcaption>
+</figure>
+<!-- <figure class="image is-128x128">
+    <img src="https://bulma.io/images/placeholders/128x128.png">
+  </figure> -->
